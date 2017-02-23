7116c4c333ce1da1aba6bd7768de72db424890a8
Alicia <Alis@perlas-mbp.attlocal.net>
2017-02-22 21:31:53 -0600

    v1.0 listens on port but showing err burger.all is not a func

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var PORT = process.env.PORT || 3000;
+var app = express();
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + "/public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+var exphbs = require("express-handlebars");
+
+//Handlebars config ---------------------------------------/
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Route config -------------------------------------------/
+var routes = require("./controllers/burgersController.js");
+
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
+
+//Database config ---------------------------------------/
+global.db = require('./models');
+
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
+
+
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
+});
