6a3695b3eee02ae90c00538ca404d2e6e8cb4f75
Alicia <Alis@PERLAs-MacBook-Pro.local>
2017-03-01 17:19:29 -0600

    change in the model and in the controller file Burger / burger

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -10,7 +10,7 @@ router.get("/", function(req, res) {
 
 router.get("/burgers", function(req, res) {
   // express callback response by calling burger.selectAllBurger
-  db.burger.findAll({}).then(function(burgerData) {
+  db.Burger.findAll({}).then(function(burgerData) {
     // dentro del ({}) puedes poner una condicion ... 
     res.render("index.handlebars", { burger_data: burgerData });
   });
@@ -19,7 +19,7 @@ router.get("/burgers", function(req, res) {
 // post route -> back to index
 router.post("/burgers/create", function(req, res) {
   // takes the request object using it as input for buger.addBurger
-  db.burger.create({
+  db.Burger.create({
 
     burger_name: req.body.burger_name,
     devoured:"false"
@@ -31,7 +31,7 @@ router.post("/burgers/create", function(req, res) {
     
 // put route -> back to index
 router.put("/burgers/:update", function(req, res) {
-  db.burger.update({
+  db.Burger.update({
 
     devoured:req.body.burger_id
   },{
